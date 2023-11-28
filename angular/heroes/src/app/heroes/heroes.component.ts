0a65d85e70e38f63336bbf9f33737a2e944a556e
Livia Oliveira <liviasoliveira03@gmail.com>
2023-11-27 20:19:14 -0400

    message

--- angular/heroes/src/app/heroes/heroes.component.ts
+++ angular/heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 
 import { Component } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from './mocks/heroes.mocks';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,8 +11,23 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero= {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[] = []
+  selectedHero?: Hero;
+
+  constructor(private  heroService: HeroService,
+    private messageService: MessageService
+    ){}
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroesMock().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelectHero(hero:Hero): void{
+    this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id ${hero.id}`)
+  }
 }
---
+++ angular/heroes/src/app/heroes/mocks/heroes.mocks.ts
@@ -0,0 +1,7 @@
+import { Hero } from "src/app/Hero";
+
+export const HEROES: Hero[] =[
+    {id: 1, name: "Batman"},
+    {id: 2, name: "Superman"},
+    {id: 3, name: "Wolverine"}
+]
