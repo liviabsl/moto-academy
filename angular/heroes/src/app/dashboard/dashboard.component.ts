de92cf5ac681b72d683a28d84e789d962932fd6c
Livia Oliveira <liviasoliveira03@gmail.com>
2023-12-01 20:25:12 -0400

    dashboard

---
+++ angular/heroes/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../Hero';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero [] = []
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesMock().subscribe(Heroes => this.heroes = Heroes.slice(1,5))
+  }
+
+}
